0cfb6e7e8bb3701ed196e3b2f268afd9954200fb
Loic-Dumas <loic.dumas02@gmail.com>
2020-03-22 02:19:05 +0100

    Remove duplicated traduction map.

--- app/src/main/java/com/loic/morseapp/morseconverter/MorseConverter.kt
+++ app/src/main/java/com/loic/morseapp/morseconverter/MorseConverter.kt
@@ -71,70 +71,7 @@ class MorseConverter {
                 'ü' to "..--"
         )
 
-
-        private val morseToLetterArray = hashMapOf(
-                //letters
-                ".-" to 'a',
-                "-..." to 'b',
-                "-.-." to 'c',
-                "-.." to 'd',
-                "." to 'e',
-                "..-." to 'f',
-                "--." to 'g',
-                "...." to 'h',
-                ".." to 'i',
-                ".---" to 'j',
-                "-.-" to 'k',
-                ".-.." to 'l',
-                "--" to 'm',
-                "-." to 'n',
-                "---" to 'o',
-                ".--." to 'p',
-                "--.-" to 'q',
-                ".-." to 'r',
-                "..." to 's',
-                "-" to 't',
-                "..-" to 'u',
-                "...-" to 'v',
-                ".--" to 'w',
-                "-..-" to 'x',
-                "-.--" to 'y',
-                "--.." to 'z',
-                // numbers
-                "-----" to '0',
-                ".----" to '1',
-                "..---" to '2',
-                "...--" to '3',
-                "....-" to '4',
-                "....." to '5',
-                "-...." to '6',
-                "--..." to '7',
-                "---.." to '8',
-                "----." to '9',
-                // symbols
-                "-.-.--" to '!',
-                ".-..-." to '"',
-                "...-..-" to '$',
-                ".----." to '\'',
-                "-.--." to '(',
-                "-.--.-" to ')',
-                ".-.-." to '+',
-                "--..--" to ',',
-                "-....-" to '-',
-                ".-.-.-" to '.',
-                "-..-." to '/',
-                "---..." to ':',
-                "-.-.-." to ',',
-                "-...-" to '=',
-                "..--.." to '?',
-                ".--.-." to '@',
-                // special letters
-                ".--.-" to 'à',
-                "..-.." to 'é',
-                ".-..-" to 'è',
-                "-.-.." to 'ç',
-                "..--" to 'ü'
-        )
+        private val morseToLetterArray = letterToMorseArray.entries.associate { (k, v) -> v to k }
 
         /**
          * @return The text given in parameter into a string of morse code.
